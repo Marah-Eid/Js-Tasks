47fb50fa902bd5d8374dc68c19fbc4c183cce34a
Marah-Eid <maraheid228@gmail.com>
2026-03-17 19:56:07 +0300

    functions

--- Arrayfunctions/task.js
+++ Arrayfunctions/task.js
@@ -219,3 +219,113 @@ let ye = findshortestWord(["apple","banana","cat","elephant"]);
 
 document.writeln(ye);
 document.writeln("<br>");
+
+/** */
+function sortByLength(arr) {
+  return arr.sort((a, b) => a.length - b.length);
+}
+document.writeln(sortByLength(["apple", "hi", "banana"]));
+document.writeln("<br>");
+
+/** */
+function removeEverySecond(arr) {
+  return arr.filter((num) => num % 2 === 0);
+}
+document.writeln(removeEverySecond([1,2,3,4,5,6]));
+document.writeln("<br>");
+
+/** */
+function sumEvenNumbers(arr) {
+  return arr
+    .filter(num => num % 2 === 0)
+    .reduce((sum, num) => sum + num, 0);
+}
+document.writeln(sumEvenNumbers([1,2,3,4,5,6]));
+document.writeln("<br>");
+
+/** */
+function sumoddindex(arr) {
+  return arr
+    .filter((num, index) => index % 2 !== 0)
+    .reduce((sum, num) => sum + num, 0);
+}
+document.writeln(sumoddindex([1,2,3,4,5,6]));
+document.writeln("<br>");
+/** */
+
+function reverseWithoutReverse(arr) {
+  let result = [];
+
+  for (let num of arr) {
+    result.unshift(num);
+  }
+
+  return result;
+}
+document.writeln(reverseWithoutReverse([1,2,3,4,5,6]));
+document.writeln("<br>");
+/** */
+function findMissingNumber(arr) {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+
+  return 0;
+}
+document.writeln(findMissingNumber([1,2,3,5,6]));
+document.writeln("<br>");
+/** */
+function pairSum(arr, target) {
+  for (let item of arr) {
+    if (arr.includes(target - item)) {
+      return [item, target - item];
+    }
+  }
+}
+document.writeln(pairSum([2, 7, 11, 15], 9));
+document.writeln("<br>");
+/** */
+function rotateArray(arr, steps) {
+  
+
+  let firstPart = arr.slice(arr.length - steps);
+  let secondPart = arr.slice(0, arr.length - steps);
+
+  return firstPart.concat(secondPart);
+}document.writeln(rotateArray([1, 2, 3, 4, 5], 2));
+document.writeln("<br>");
+/** */
+function chunkArray(arr, size) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+
+  return result;
+}
+document.writeln(chunkArray([1,2,3,4,5,6], 2));
+document.writeln("<br>");
+
+
+function groupByLength(arr) {
+  let result = {};
+
+  for (let word of arr) {
+    let len = word.length;
+    result[len] = result[len] || [];
+    result[len].push(word);
+  }
+
+  return result;
+}
+
+let outut = groupByLength(["hi","cat","dog","apple"]);
+
+for (let key in outut) {
+  document.writeln(key + ": [" + outut[key].join(", ") + "]<br>");
+}
